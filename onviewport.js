be611f94483815f7239b049d89223be48e3a73bf
Ignacio Chavez <nashio@gmail.com>
2013-11-27 15:57:46 -0800

    added example

--- onviewport.js
+++ onviewport.js
@@ -3,27 +3,27 @@
 // http://github.com/nashio
 
 (function(w){ 
-	$.fn.onViewPort = function(options, callback) {
-		var settings = $.extend({}, options );
-				self = this;
-		var isElementInViewport = function(){
-			var box = this.getBoundingClientRect();
-			return (
-			    box.top >= 0 &&
-			    box.left >= 0 &&
-			    box.bottom <= (window.innerHeight || $(window).height() ) && 
-			    box.right <= (window.innerWidth ||  $(window).width() ) 
-			    );
-		};
-	  $(window).on("scroll", function(){
-		  self.each(function() {
-	    	if( isElementInViewport.call(this) ) {
-	    		if( typeof callback === "function" ){
-	    			callback();
-	    		}
-	    	}
-	    });
-	   });
+  $.fn.onViewPort = function(options, callback) {
+    var settings = $.extend({}, options );
+        self = this;
+    var isElementInViewport = function(){
+      var box = this.getBoundingClientRect();
+      return (
+          box.top >= 0 &&
+          box.left >= 0 &&
+          box.bottom <= (window.innerHeight || $(window).height() ) && 
+          box.right <= (window.innerWidth ||  $(window).width() ) 
+          );
+    };
+    $(window).on("scroll", function(){
+      self.each(function() {
+        if( isElementInViewport.call(this) ) {
+          if( typeof callback === "function" ){
+            callback();
+          }
+        }
+      });
+     });
 
   };
 
