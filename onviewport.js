903ec26a5b4ffa6d1e964e2b74d45e60b0b1668d
Ignacio Chavez <nashio@gmail.com>
2013-11-27 15:45:28 -0800

    initial commit

---
+++ onviewport.js
@@ -0,0 +1,30 @@
+// onViewPort
+// Execute a function when a certain element is in the viewport
+// http://github.com/nashio
+
+(function(w){ 
+	$.fn.onViewPort = function(options, callback) {
+		var settings = $.extend({}, options );
+				self = this;
+		var isElementInViewport = function(){
+			var box = this.getBoundingClientRect();
+			return (
+			    box.top >= 0 &&
+			    box.left >= 0 &&
+			    box.bottom <= (window.innerHeight || $(window).height() ) && 
+			    box.right <= (window.innerWidth ||  $(window).width() ) 
+			    );
+		};
+	  $(window).on("scroll", function(){
+		  self.each(function() {
+	    	if( isElementInViewport.call(this) ) {
+	    		if( typeof callback === "function" ){
+	    			callback();
+	    		}
+	    	}
+	    });
+	   });
+
+  };
+
+})(window);
