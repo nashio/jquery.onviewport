149a8eb7a7edf07ed115f1d260ce90b1f28506eb
Ignacio Chavez <nashio@gmail.com>
2014-08-23 11:29:33 -0700

    Update onviewport.js

--- onviewport.js
+++ onviewport.js
@@ -1,6 +1,7 @@
-// onViewPort
+// onViewPort (c) 2013, Ignacio Chavez. http://github.com/nashio
 // Executes a function when a certain element is within the boundries of the viewport
-// http://github.com/nashio...
+
+
 (function(w){ 
   $.fn.onViewPort = function(options, callback) {
     var settings = $.extend({}, options );
